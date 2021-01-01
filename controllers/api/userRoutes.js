033516403e6e90459431c2da41129e8800de6855
Camila Alves Meyer <camila.alves85@gmail.com>
2021-01-02 02:05:46 +0800

    Created userRoutes

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      // Sucess response 200 - OK
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    // Client error response 400 - Bad request
+    res.status(400).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        // Client error response 400 - Bad request
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        // Client error response 400 - Bad request
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    // Client error response 400 - Bad request
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      // Sucess response 204 - No Content
+      res.status(204).end();
+    });
+  } else {
+    // Client error response 404 - Not found
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
