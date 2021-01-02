3327dc03a9bf152092685b5b4fb0a567394818ed
Camila Alves Meyer <camila.alves85@gmail.com>
2021-01-02 16:54:48 +0800

    Removed deleted routes path

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
-const editPostRoutes = require('./editPostRoutes');
-const commentRoutes = require('./commentRoutes');
-
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
-router.use('/edit-post', editPostRoutes);
-router.use('/comment', commentRoutes);
 
 module.exports = router;
